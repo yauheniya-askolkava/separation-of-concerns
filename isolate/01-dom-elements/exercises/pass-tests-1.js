c07e243bcab48c7c548aff27907d711832dfc93d
yauh-ask <yauheniya.askolkava@gmail.com>
2020-08-01 18:07:23 +0200

    01_ex_isolate

--- isolate/01-dom-elements/exercises/pass-tests-1.js
+++ isolate/01-dom-elements/exercises/pass-tests-1.js
@@ -1,12 +1,12 @@
-'use strict';
-
-
-
+"use strict";
+const element = document.createElement("input");
+element.placeholder = "enter your username";
+element.className = "auth-field";
 
 // the assertions are correct!  write code above to pass them
-console.assert(element.nodeName === 'INPUT',
-  'Test 1: nodeName');
-console.assert(element.placeholder === 'enter your username',
-  'Test 2: placeholder');
-console.assert(element.className === 'auth-field',
-  'Test 3: className');
+console.assert(element.nodeName === "INPUT", "Test 1: nodeName");
+console.assert(
+  element.placeholder === "enter your username",
+  "Test 2: placeholder"
+);
+console.assert(element.className === "auth-field", "Test 3: className");
--- isolate/01-dom-elements/exercises/pass-tests-2.js
+++ isolate/01-dom-elements/exercises/pass-tests-2.js
@@ -1,12 +1,9 @@
-'use strict';
-
-
-
+"use strict";
+const element = document.createElement("p");
+element.innerHTML = "lorem ipsum dolor";
+element.className = "fancy-text";
 
 // the assertions are correct!  write code above to pass them
-console.assert(element.nodeName === 'P',
-  'Test 1: nodeName');
-console.assert(element.innerHTML === 'lorem ipsum dolor',
-  'Test 2: innerHTML');
-console.assert(element.className === 'fancy-text',
-  'Test 3: className');
+console.assert(element.nodeName === "P", "Test 1: nodeName");
+console.assert(element.innerHTML === "lorem ipsum dolor", "Test 2: innerHTML");
+console.assert(element.className === "fancy-text", "Test 3: className");
